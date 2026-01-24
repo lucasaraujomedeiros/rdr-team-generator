c4ba3665d23622d23319440ad9d99644999641c2
lucasaraujomedeiros <lucas.araujo.medeiros@ccc.ufcg.edu.br>
2026-01-24 15:21:09 -0300

    separacao entre o main e arquivos de logica

--- app/Main.hs
+++ app/Main.hs
@@ -1,44 +1,9 @@
+module Main where
+
+import Sorteador
 import Data.List (sortBy, minimumBy)
 import Data.Ord (comparing, Down (Down))
 import System.IO (hFlush, stdout)
-
-data Jogador = Jogador {
-    nomeJogador :: String,
-    estrelas :: Int
-} deriving (Show, Eq)
-
-data Time = Time {
-    nomeTime :: String,
-    elenco :: [Jogador],
-    forcaTotal :: Int
-} deriving (Show)
-
-
-montarTimesEquilibrados :: Int -> Int -> [Jogador] -> [Time]
-montarTimesEquilibrados qtdTimes maxPorTime jogadores =
-    let
-        -- Ordenar decrescente por estrelas do melhor pro pior
-        jogadoresOrdenados = sortBy (comparing (Down . estrelas)) jogadores
-
-        -- Inicializar times vazios
-        inicializarTimes = map (\n -> Time { nomeTime = "Time " ++ show n, elenco = [], forcaTotal = 0 }) [1..qtdTimes]
-
-        -- Atribui um jogador ao time disponível (len < maxPorTime) com menor força total e, em caso de empate, menor elenco
-        assignPlayer :: Jogador -> [Time] -> [Time]
-        assignPlayer jog timesList =
-            let availIdxs = [ i | (t,i) <- zip timesList [0..], length (elenco t) < maxPorTime ]
-            in if null availIdxs
-               then timesList -- todos os times cheios; jogador não é atribuído
-               else
-                   let candidates = [ (forcaTotal (timesList !! i), length (elenco (timesList !! i)), i) | i <- availIdxs ]
-                       (_, _, idx) = minimumBy (comparing (\(ft,len,i) -> (ft,len,i))) candidates
-                       (before, t:after) = splitAt idx timesList
-                       t' = t { elenco = elenco t ++ [jog], forcaTotal = forcaTotal t + estrelas jog }
-            in before ++ (t' : after)
-
-        -- Construir times aplicando o algoritmo guloso
-        timesFinal = foldl (flip assignPlayer) inicializarTimes jogadoresOrdenados
-        in timesFinal
 
 -- Função para ler tudo numa mesma linha
 prompt :: String -> IO String
@@ -46,6 +11,7 @@ prompt texto = do
     putStr texto
     hFlush stdout
     getLine
+
 
 -- Construir lista com os jogadores cadastrados
 cadastrarJogadores :: Int -> IO [Jogador]
