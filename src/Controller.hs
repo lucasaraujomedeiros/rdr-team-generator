6868b9cc508173cba252fcb463e9c2bbbbd56677
gleydson <gleydsonfabricio3@gmail.com>
2026-01-25 11:16:50 -0300

    feat: controller da logica de sistema

---
+++ src/Controller.hs
@@ -0,0 +1,47 @@
+module Controller where
+
+import CadastraJogadores (cadastrarJogadores)
+import Sorteador (montarTimesEquilibrados, Time(..), Jogador(..)) -- importar depois tambem o não deterministico
+import MataMata (torneio)
+
+cadastrarJogadoresController :: IO ()
+cadastrarJogadoresController = cadastrarJogadores 1
+
+listarJogadoresController :: IO ()
+listarJogadoresController = do 
+    putStrLn "Listando Jogadores" -- TODO: função que lista os jogadores do .TXT
+
+selecionarJogadoresController :: IO [Jogador]
+selecionarJogadoresController = do
+    putStr "jogadores selecionados -> " -- TODO: chamar função selecionar jogadores do 'CadastraJogadores'
+    return []
+    
+sorteioRapidoController :: [Jogador] -> IO [Time]
+sorteioRapidoController js = do
+    let numTimes = 3 -- fazer prompt para pegar esses dados
+    let numJogPorTime = 2
+    let times = montarTimesEquilibrados numTimes numJogPorTime js 
+    putStrLn (show times)
+    return []
+
+sorteioNaoDeterministicoController :: [Jogador] -> IO [Time]
+sorteioNaoDeterministicoController js = do 
+    putStrLn "rodando o não deterministico" -- TODO:  implementar a função que roda o não deterministico
+    return []
+
+pontosCorridosController :: [Time] -> IO ()
+pontosCorridosController ts = do
+    putStrLn "rodando os pontos corridos" -- TODO: implementar a função que roda o pontos corridos
+
+mataMataController :: [Time] -> IO ()
+mataMataController ts = torneio [nomeTime t | t <- ts]
+
+campeonatoCompletoController :: [Time] -> IO ()
+campeonatoCompletoController ts = do
+    let timesAposPontosCorridos = [] -- chamar os pontos corridos
+    torneio [nomeTime t | t <- timesAposPontosCorridos]
+
+
+
+
+
